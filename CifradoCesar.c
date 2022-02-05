e95328a24508a9a8fcf8f1dd28d9e6c9395e2559
Alejandro Lujan <315195907@pcpuma.acatlan.unam.mx>
2022-02-05 13:32:37 -0600

    menu y abecesario

--- CifradoCesar.c
+++ CifradoCesar.c
@@ -2,15 +2,48 @@
         Tarea 01: Cifrado César
         Materia: Seguridad Computacional
         Autor: César Alejandro Luján García
-        Porfesor: RRodriguez Arana Ricardo
-
+        Porfesor: Rodriguez Arana Ricardo
 */
 
-include<stdio.h>
+#include <stdio.h>
+
+
+int menu(){
+
+    short opcion;
+    printf("\nHola, las opciones a realizar son:");
+    printf("\n1. Cifrar un texto.");
+    printf("\n2. Decifrar un texto.");
+    printf("\n3. Salir");
+    
+
+    do
+    {
+       printf("\nHola ingresa el número entero correspondiente a la opción que quieres: ");
+       scanf("%d",&opcion);
+    } while (opcion != 3);
+    
+    return opcion;
+
+}
+
+void cifrado(){
+
+}
+
+
+void decifrado(){
+
+}
+
 
 
 int main(){
 
+    int opcion;
+    char abedecarioIngles[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    opcion = menu();
 
 
     return 0;
